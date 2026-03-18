808ace4df8c9fb104dcb7502e05408603890b465
Jitey <jt.bousseau@gmail.com>
2026-03-18 16:52:59 +0100

    Initial Commit

---
+++ FuelTracker/FuelTrackerApp.swift
@@ -0,0 +1,32 @@
+//
+//  FuelTrackerApp.swift
+//  FuelTracker
+//
+//  Created by Jitey on 18/03/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FuelTrackerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
